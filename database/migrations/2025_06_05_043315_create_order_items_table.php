20b5f53ae014d47a1edb1343edf1564cb0382bce
RafalBytniewski <rafal.bytniewski@o2.pl>
2025-06-05 20:30:34 +0200

    Added model and migration files

---
+++ database/migrations/2025_06_05_043315_create_order_items_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreingId('order_id')->constrained('orders')->cadcadeOnDelete();
+            $table->foreingId('product_id')->constrained('products')->cadcadeOnDelete();
+            $table->integer('quantity')->default(1);
+            $table->decimal('unit_ammout', 10, 2)->nullable();
+            $table->decimal('total_amount', 10, 2)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_items');
+    }
+};
